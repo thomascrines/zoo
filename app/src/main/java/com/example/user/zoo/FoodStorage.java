f36263dd94967275be39ff95dea1a08f20b467be
Thomas Crines <thomascrines@hotmail.com>
2016-11-13 18:42:48 +0000

    add and remove food from food storage and tests

--- app/src/main/java/com/example/user/zoo/FoodStorage.java
+++ app/src/main/java/com/example/user/zoo/FoodStorage.java
@@ -1,31 +1,35 @@
-//package com.example.user.zoo;
-//
-//import java.util.ArrayList;
-//
-///**
-// * Created by user on 13/11/2016.
-// */
-//
-//public class FoodStorage {
-//
-//    public ArrayList<Food> totalFood;
-//
-//    public FoodStorage() {
-//
-//        this.totalFood = new ArrayList<>();
-//    }
-//
-//    public ArrayList<Food> getTotalFood() {
-//        return totalFood;
-//    }
-//
-//    public void addFood(Food food, int quantity) {
-//        totalFood.add(food);
-//    }
-//
-//    public void removeFood(Food food) {
-//        totalFood.remove(food);
-//    }
-//
-//}
+package com.example.user.zoo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class FoodStorage {
+
+    public ArrayList<Food> totalFood;
+
+    public FoodStorage() {
+
+        this.totalFood = new ArrayList<>();
+    }
+
+    public ArrayList<Food> getTotalFood() {
+        return totalFood;
+    }
+
+    public void addFood(Food food, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            totalFood.add(food);
+        }
+    }
+
+    public void removeFood(Food food, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            totalFood.remove(food);
+        }
+    }
+
+}
 
--- app/src/test/java/com/example/user/zoo/FoodStorageTest.java
+++ app/src/test/java/com/example/user/zoo/FoodStorageTest.java
@@ -1,8 +1,40 @@
-//package com.example.user.zoo;
-//
-///**
-// * Created by user on 13/11/2016.
-// */
-//
-//public class FoodStorageTest {
-//}
+package com.example.user.zoo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 13/11/2016.
+ */
+
+public class FoodStorageTest {
+
+    FoodStorage foodStorage;
+    Food peanuts;
+    Food grains;
+
+    @Before
+    public void before() {
+        foodStorage = new FoodStorage();
+        peanuts = new Food("Peanuts", 0.55, Diet.PEANUTS);
+        grains = new Food("Grains", 0.42, Diet.GRAINS);
+    }
+
+    @Test
+    public void testCanAddFood() {
+        foodStorage.addFood(peanuts, 3);
+        foodStorage.addFood(grains, 1);
+        assertEquals(4, foodStorage.getTotalFood().size());
+    }
+
+    @Test
+    public void canRemoveFood() {
+        foodStorage.addFood(peanuts, 6);
+        foodStorage.addFood(grains, 3);
+        foodStorage.removeFood(peanuts,2);
+        foodStorage.removeFood(grains,1);
+        assertEquals(6, foodStorage.getTotalFood().size());
+    }
+}
